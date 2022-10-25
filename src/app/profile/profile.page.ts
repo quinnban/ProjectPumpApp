71e22d37cf48d67ac10b6b716ebdb393de678d56
Quinn <quinn.bancescu@gmail.com>
2022-10-25 17:33:19 -0600

    added photo stuff

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
+  pictureUrl=null;
+
   constructor() { }
 
+
+  async changePicture(){
+    const picture = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    });
+
+    this.pictureUrl = picture.webPath;
+    console.log(picture);
+
+    const readSecretFile = await Filesystem.readFile({
+      path: picture.webPath,
+    });
+    console.log(readSecretFile);
+  }
+
   ngOnInit() {
+    //https://capacitorjs.com/docs/apis/camera#pwa-notes
+    //https://capacitorjs.com/docs/web/pwa-elements
+
+    /*
+      const takePicture = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    });
+
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    var imageUrl = image.webPath;
+
+    // Can be set to the src of an image now
+    imageElement.src = imageUrl;
+  };
+*/
   }
 
 }
