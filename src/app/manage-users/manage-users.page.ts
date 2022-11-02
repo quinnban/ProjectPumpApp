3b216a89dff0c89540e7ac77d05579342d951897
Quinn <quinn.bancescu@gmail.com>
2022-11-02 09:47:15 -0600

    add pop up to invite user

--- src/app/manage-users/manage-users.page.ts
+++ src/app/manage-users/manage-users.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-manage-users',
@@ -10,9 +11,39 @@ export class ManageUsersPage implements OnInit {
   user = Array(20).fill(0).map((x,i)=>i);
 
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async inviteUser(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Invite a user',
+      buttons: [
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: (value) => {
+            console.log(value);
+            //do http return false is error
+            //return false;
+          },
+      },
+      {
+        role:'cancel',
+        text:'cancel'
+      }
+    ],
+      inputs: [
+        {
+          placeholder: 'Email',
+          type:'email'
+        },
+      ],
+    });
+
+    await alert.present();
+
   }
 
 }
