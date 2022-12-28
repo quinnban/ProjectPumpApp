1ed1a77cc1904233c0080fc1813d7fecfc138923
Quinn <quinn.bancescu@gmail.com>
2022-12-28 09:22:33 -0600

    add temp video

--- src/app/workout-details/components/exercise-card/exercise-card.component.ts
+++ src/app/workout-details/components/exercise-card/exercise-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, ElementRef, Inject, Input, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 import { ExerciseDetail } from 'src/app/shared/models/exerciseDetail.model';
 
 @Component({
@@ -9,13 +11,24 @@ import { ExerciseDetail } from 'src/app/shared/models/exerciseDetail.model';
 export class ExerciseCardComponent implements OnInit {
 
   @Input() exercise: ExerciseDetail;
+  videoWidth;
 
-  constructor() { }
+  constructor(public platform: Platform, @Inject(DOCUMENT) private document: Document) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log(this.platform.width());
+    this.videoWidth = this.platform.width()-32;
+  }
 
   userclick(ev: any){
     console.log(ev);
+  }
+
+  openFullscreen(element){
+    console.log(this.document.fullscreenEnabled);
+    if(this.document.fullscreenEnabled){
+      element.requestFullscreen();
+    }
   }
 
 }
